c6765acd1c10a9a75160b9a106e8b7d05094584e
arkan.gilang <arkan.gilang@traveloka.com>
2022-04-17 00:55:36 +0700

    prop_index

--- src/Edge.ts
+++ src/Edge.ts
@@ -22,26 +22,6 @@ export default class Edge extends BasicGraphObject {
         else this.target = target;
         this.edgeType = edgeType;
     }
-
-    setProp(key: string, value: string): void {
-        if(!this.props) this.props = new BasicProps(key, value);
-        else this.props.set(key,value);
-        if(this.hook) this.hook.callHook("addProp",this,key);
-    }
-    getProp(key: string): string {
-        if(!this.props) return;
-        return this.props.get(key);
-    }
-    deleteProp(key: string): boolean {
-        if(!this.props) return false;
-        if(Object.keys(this.props.items).length==1 && typeof this.props.get(key) !== "undefined") 
-            return delete this.props;
-        if(this.hook) this.hook.callHook("removeProp",this,key);
-        return this.props.delete(key);
-    }
-    getProps(): Props {
-        return this.props;
-    }
     
     serialize(): string {
         return super.serialize();
