82a103ea37587ed8e3ce66212027cde7b9f530f5
arkan.gilang <arkan.gilang@traveloka.com>
2022-04-24 21:54:14 +0700

    fix codesmell

--- src/BasicGraphObject.ts
+++ src/BasicGraphObject.ts
@@ -1,7 +1,5 @@
-import { Pair } from "yaml/types";
 import Hookable from "./Hookable";
 import { Propable, BasicProps, Props } from "./Propable";
-import YAML from "yaml";
 const keyIndex=["id","class","vIn","vOut","edgeType","source","target","props"];
 
 
@@ -28,18 +26,4 @@ export default abstract class BasicGraphObject implements Propable {
         return this.props;
     }
     abstract getId(): string;
-    // serialize(): string {
-    //     let tempHook = this.hook;
-    //     delete this.hook;
-    //     let result = YAML.stringify(this, {sortMapEntries: (a: Pair, b:Pair): number=>{
-    //         let a1 = keyIndex.indexOf(a.key.value);
-    //         let b1 = keyIndex.indexOf(b.key.value);
-    //         if(a1 > -1 && b1 > -1)
-    //             return a1-b1;
-    //         else if(a>b) return 1 
-    //              else return -1
-    //     }});
-    //     this.hook = tempHook;
-    //     return result;
-    // }
 }
